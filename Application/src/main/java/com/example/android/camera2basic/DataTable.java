ae98af5a1fdd0a18ca35d5133e833457212e8334
Daniel Ruwe <baddavegecko1@gmail.com>
2016-02-27 19:00:44 -0500

    update

--- Application/src/main/java/com/example/android/camera2basic/DataTable.java
+++ Application/src/main/java/com/example/android/camera2basic/DataTable.java
@@ -5,9 +5,18 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import com.reimaginebanking.api.java.NessieClient;
+import com.reimaginebanking.api.java.NessieException;
+import com.reimaginebanking.api.java.NessieResultsListener;
+import com.reimaginebanking.api.java.models.Customer;
+import com.reimaginebanking.api.java.models.Bill;
 
 import java.util.List;
 
@@ -28,6 +37,8 @@ public class DataTable extends Activity {
      */
     private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
 
+    Table_Adapter myAdapter;
+
     /**
      * Some older devices needs a small delay between UI widget updates
      * and a change of the status and navigation bar.
@@ -35,23 +46,7 @@ public class DataTable extends Activity {
     private static final int UI_ANIMATION_DELAY = 300;
     private final Handler mHideHandler = new Handler();
     private View mContentView;
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
 
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
-    };
     private View mControlsView;
     private final Runnable mShowPart2Runnable = new Runnable() {
         @Override
@@ -98,16 +93,65 @@ public class DataTable extends Activity {
 
 
         // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
+        /*mContentView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 toggle();
             }
-        });
+        });*/
 
         //mystical for loop of happiness
-        ListView newView = (ListView)findViewById(R.id.lv_sadness);
-        TableItem tempItem = new TableItem();
+        final ListView newView = (ListView)findViewById(R.id.lv_sadness);
+        //TableItem tempItem = new TableItem();
+        NessieClient nessieClient = NessieClient.getInstance();
+        nessieClient.setAPIKey("ba7c2af9690af2027d775938b9e9ea83");
+
+        /*nessieClient.getCustomers(new NessieResultsListener() {
+            @Override
+            public void onSuccess(Object result, NessieException e) {
+                Log.i("info", "PETER onSuccess");
+                if (e == null) {
+                    // No errors
+                    ArrayList<Customer> customers = (ArrayList<Customer>) result;
+                    Log.i("info", "PETER" + customers.get(0).getFirst_name());
+                } else {
+                    Log.e("getCustomers Nessie", e.toString());
+                }
+            }
+        });
+        nessieClient.getAccount("56c66be6a73e492741507e36", new NessieResultsListener() {
+            @Override
+            public void onSuccess(Object result, NessieException e) {
+                Log.i("info", "PETER onSuccess");
+                if (e == null) {
+                    // No errors
+                    Log.i("info", "GotAccount");
+                } else {
+                    Log.e("getAccount Nessie", e.toString());
+                }
+            }
+        });*/
+
+        Log.i("info", "calling getBills...");
+        nessieClient.getBills("56c66be6a73e492741507e36", new NessieResultsListener() {
+            @Override
+            public void onSuccess(Object result, NessieException e) {
+                Log.i("info", "getBills response...");
+                if (e == null) {
+                    ArrayList<Bill> bills = (ArrayList<Bill>) result;
+                    System.out.println("BILLS TEST");
+                    System.out.println(bills.size());
+                    myAdapter = new Table_Adapter(getApplicationContext(), bills);
+                    newView.setAdapter(myAdapter);
+                    for (Bill bill : bills) {
+                        System.out.println(bill.toString());
+                    }
+                } else {
+                    System.out.println(e.toString());
+                    Log.i("info", "Nessie Bill exception");
+                }
+            }
+        });
 
 
         // Upon interacting with UI controls, delay any scheduled hide()
@@ -140,12 +184,9 @@ public class DataTable extends Activity {
         if (actionBar != null) {
             actionBar.hide();
         }
-        mControlsView.setVisibility(View.GONE);
-        mVisible = false;
 
         // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
+
     }
 
     @SuppressLint("InlinedApi")
@@ -155,9 +196,6 @@ public class DataTable extends Activity {
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
         mVisible = true;
 
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
     }
 
     /**
